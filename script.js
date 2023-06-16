89c51d4526cd404dad075c87ae75a979aab686d0
Freddierezende <freddie.rezende.2003@gmail.com>
2023-06-16 17:09:45 -0300

    Tarefa 1 - Acrescentando declaração const/let nas variáveis

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Variáveis para aplicar os resultados do teste. Você deverá categoriza-las como let/const, mas não deve alterar os valores.
-gender = process.argv[2]; // Genero 
-height = process.argv[3]; // Altura
-barReps = process.argv[4]; // Repetições com barra
-barSeconds = process.argv[5]; // Tempo das repetições com barra 
-abs = process.argv[6]; // Abdominais
-runDistance = process.argv[7]; // Distância da corrida
-runTime = process.argv[8]; // Tempo da corrida
-swimDistance = process.argv[9]; // Distância da natação
-swimTime = process.argv[10]; // Tempo da natação
-diveTime = process.argv[11]; // Tempo de mergulho
+let gender = process.argv[2]; // Genero 
+let height = process.argv[3]; // Altura
+let barReps = process.argv[4]; // Repetições com barra
+let barSeconds = process.argv[5]; // Tempo das repetições com barra 
+let abs = process.argv[6]; // Abdominais
+let runDistance = process.argv[7]; // Distância da corrida
+let runTime = process.argv[8]; // Tempo da corrida
+let swimDistance = process.argv[9]; // Distância da natação
+let swimTime = process.argv[10]; // Tempo da natação
+let diveTime = process.argv[11]; // Tempo de mergulho
 
-passed = false;
+let passed = false;
 
 /*
     Importante:
