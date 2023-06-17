39615ac20782bdb3f491152215a742f2f50708a6
Freddierezende <freddie.rezende.2003@gmail.com>
2023-06-17 15:28:49 -0300

    Tarefa 2 - Estruturas condicionais e operadores

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Variáveis para aplicar os resultados do teste. Você deverá categoriza-las como let/const, mas não deve alterar os valores.
-let gender = process.argv[2]; // Genero 
+const gender = process.argv[2]; // Genero 
 let height = process.argv[3]; // Altura
 let barReps = process.argv[4]; // Repetições com barra
 let barSeconds = process.argv[5]; // Tempo das repetições com barra 
@@ -12,6 +12,8 @@ let diveTime = process.argv[11]; // Tempo de mergulho
 
 let passed = false;
 
+
+
 /*
     Importante:
     Se o genero (gender) for masculino, utilize a palavra "male"
@@ -22,6 +24,47 @@ let passed = false;
     ...
 */
 // **sua lógica a partir daqui**
+
+if (gender === "male"){
+    // Caso o candidato for do gênero masculino
+ if (height >= 1.70) {
+    // caso tenha altura seja maior ou igual a 1.50 metros
+ if (barReps >= 6 || barSeconds >= 15){
+    // Caso realize no minímo 6 repetições ou consiga fazer no mínimo 15 segundo de barra
+ if (abs >= 41){
+    // Caso realize 41 ou mais abdominais
+if ((runDistance >= 3000 && runTime <= 720) || (runDistance >= 5000 && runTime <= 1200)) {
+    // Caso realize 3km ou mais em no máximo 12 minutos, ou 5km ou mais em no máximo 20 minutos 
+ if ((swimDistance >= 100 && swimTime <= 60) ||(diveTime <= 30)) {
+    // Distância comparando tempo de natação ou realize o tempo de mergulho estimado
+ 
+passed = true; 
+
+                    }
+                }
+            }
+        }
+    }
+}  else if (gender === "female") {
+    // Caso o candidato for do gênero masculino 
+ if (height>= 1.60){
+    // Caso a altura seja maior ou igual 1.60 metros
+ if(barReps >= 5 || barSeconds >= 12 ){
+    // Caso realize no mínimo 5 repetições ou consiga fazer no mínimo 12 segundos de barra 
+ if (abs >= 41){
+    // Caso realize 41 ou mais abdominais
+ if((runDistance >= 4000 && runTime <= 900) || (runDistance >= 6000 && runTime <= 1320)){
+    // Caso realize 4km ou mais em no máximo 15 minutos, ou 6km ou mais em no máximo 22 minutos 
+ if ((swimDistance >=100 && swimTime <= 60) || (diveTime <= 30)) {
+    // Distância comparando tempo de natação ou realize o tempo de mergulho estimado
+
+    passed = true;
+                     }
+                }
+            }
+        }
+    }
+ }
 
 
 
