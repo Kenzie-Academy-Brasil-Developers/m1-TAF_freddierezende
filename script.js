08d49aed941400ccce9bab34cd67402ce9ea0df8
Freddierezende <freddie.rezende.2003@gmail.com>
2023-06-19 14:40:48 -0300

    TAF - entrega final

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Variáveis para aplicar os resultados do teste. Você deverá categoriza-las como let/const, mas não deve alterar os valores.
 const gender = process.argv[2]; // Genero 
-let height = process.argv[3]; // Altura
-let barReps = process.argv[4]; // Repetições com barra
-let barSeconds = process.argv[5]; // Tempo das repetições com barra 
-let abs = process.argv[6]; // Abdominais
-let runDistance = process.argv[7]; // Distância da corrida
-let runTime = process.argv[8]; // Tempo da corrida
-let swimDistance = process.argv[9]; // Distância da natação
-let swimTime = process.argv[10]; // Tempo da natação
-let diveTime = process.argv[11]; // Tempo de mergulho
+const height = process.argv[3]; // Altura
+const barReps = process.argv[4]; // Repetições com barra
+const barSeconds = process.argv[5]; // Tempo das repetições com barra 
+const abs = process.argv[6]; // Abdominais
+const runDistance = process.argv[7]; // Distância da corrida
+const runTime = process.argv[8]; // Tempo da corrida
+const swimDistance = process.argv[9]; // Distância da natação
+const swimTime = process.argv[10]; // Tempo da natação
+const diveTime = process.argv[11]; // Tempo de mergulho
 
 let passed = false;
 
